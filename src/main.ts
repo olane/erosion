5cfdf64de84c89cb6361a1f2f4cb163d3d041230
Oli Lane <olil4ne@gmail.com>
2026-07-12 10:41:23 +0100

    upgrade phaser from v3 to v4.2.1

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ const config: Phaser.Types.Core.GameConfig = {
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
   render: {
-    roundPixels: true,
+    roundPixels: false,
     antialias: true,
   },
 };
